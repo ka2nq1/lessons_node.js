8b5fbba0bed90c9144ae5296e35f957b401dacd5
ka2nq1 <ka2nq1@gmail.com>
2021-03-27 14:10:33 +0200

    ignor + env

--- lesson 3_express/simple-express/routes/index.js
+++ lesson 3_express/simple-express/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body
+  res.render('response', { title: 'Simple express app', email, password })
+})
+
 module.exports = router;
